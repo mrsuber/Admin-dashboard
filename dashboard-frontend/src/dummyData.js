a7644223d3b9e08c1af34edcca24818776848208
mohamad <msiysinyuy@gmail.com>
2021-09-04 20:52:39 +0100

    the small widget done

---
+++ dashboard-frontend/src/dummyData.js
@@ -0,0 +1,62 @@
+export const userData = [
+{
+  name: 'January',
+  "Active User": 4000,
+
+},
+{
+  name: 'February',
+  "Active User": 3000,
+
+},
+{
+  name: 'March',
+  "Active User": 2000,
+
+},
+{
+  name: 'April',
+  "Active User": 2780,
+
+},
+{
+  name: 'May',
+  "Active User": 1890,
+
+},
+{
+  name: 'June',
+  "Active User": 2390,
+
+},
+{
+  name: 'July',
+  "Active User": 3490,
+
+},
+{
+  name: 'August',
+  "Active User": 3590,
+
+},
+{
+  name: 'September',
+  "Active User": 2290,
+
+},
+{
+  name: 'October',
+  "Active User": 2000,
+
+},
+{
+  name: 'November',
+  "Active User": 4000,
+
+},
+{
+  name: 'December',
+  "Active User": 4500,
+
+},
+];
