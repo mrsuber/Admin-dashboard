f3a5467fd45fde397309dfc70d112b0eea36770a
mohamad <msiysinyuy@gmail.com>
2021-09-05 10:27:36 +0100

    user page created

--- dashboard-frontend/src/App.js
+++ dashboard-frontend/src/App.js
@@ -2,19 +2,34 @@ import Topbar from './components/topbar/Topbar'
 import Sidbar from './components/sidebar/Sidebar'
 import './app.css'
 import Home from "./pages/home/Home"
+import UserList from "./pages/userList/UserList"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+}from "react-router-dom"
 
 
 
 
 function App() {
   return (
-    <div>
+    <Router>
       <Topbar/>
       <div className="container">
         <Sidbar />
-        <Home />
+        <Switch>
+            <Route exact path="/">
+                <Home />
+            </Route>
+            <Route exact path="/users">
+                <UserList />
+            </Route>
+
+        </Switch>
+
       </div>
-    </div>
+    </Router>
   );
 }
 
--- dashboard-frontend/src/dummyData.js
+++ dashboard-frontend/src/dummyData.js
@@ -60,3 +60,18 @@ export const userData = [
 
 },
 ];
+
+
+export const userRows = [
+  { id: 1, username: 'mrsuber',avatar:"https://i.pinimg.com/originals/af/1c/c3/af1cc3fa780eddc7c91a70f9e836b12c.jpg",email:"msiysinyuy@gmaail.com",status:"active",transaction:"$120.00"},
+  { id: 2, username: 'mrsuber',avatar:"https://i.pinimg.com/originals/af/1c/c3/af1cc3fa780eddc7c91a70f9e836b12c.jpg",email:"msiysinyuy@gmaail.com",status:"active",transaction:"$120.00"},
+  { id: 3, username: 'mrsuber',avatar:"https://i.pinimg.com/originals/af/1c/c3/af1cc3fa780eddc7c91a70f9e836b12c.jpg",email:"msiysinyuy@gmaail.com",status:"active",transaction:"$120.00"},
+  { id: 4, username: 'mrsuber',avatar:"https://i.pinimg.com/originals/af/1c/c3/af1cc3fa780eddc7c91a70f9e836b12c.jpg",email:"msiysinyuy@gmaail.com",status:"active",transaction:"$120.00"},
+  { id: 5, username: 'mrsuber',avatar:"https://i.pinimg.com/originals/af/1c/c3/af1cc3fa780eddc7c91a70f9e836b12c.jpg",email:"msiysinyuy@gmaail.com",status:"active",transaction:"$120.00"},
+  { id: 6, username: 'mrsuber',avatar:"https://i.pinimg.com/originals/af/1c/c3/af1cc3fa780eddc7c91a70f9e836b12c.jpg",email:"msiysinyuy@gmaail.com",status:"active",transaction:"$120.00"},
+  { id: 7, username: 'mrsuber',avatar:"https://i.pinimg.com/originals/af/1c/c3/af1cc3fa780eddc7c91a70f9e836b12c.jpg",email:"msiysinyuy@gmaail.com",status:"active",transaction:"$120.00"},
+  { id: 8, username: 'mrsuber',avatar:"https://i.pinimg.com/originals/af/1c/c3/af1cc3fa780eddc7c91a70f9e836b12c.jpg",email:"msiysinyuy@gmaail.com",status:"active",transaction:"$120.00"},
+  { id: 9, username: 'mrsuber',avatar:"https://i.pinimg.com/originals/af/1c/c3/af1cc3fa780eddc7c91a70f9e836b12c.jpg",email:"msiysinyuy@gmaail.com",status:"active",transaction:"$120.00"},
+  { id: 10, username: 'mrsuber',avatar:"https://i.pinimg.com/originals/af/1c/c3/af1cc3fa780eddc7c91a70f9e836b12c.jpg",email:"msiysinyuy@gmaail.com",status:"active",transaction:"$120.00"},
+
+  ];
