ce9140cee0f49531cccc67f97f309800af26af6e
mohamad <msiysinyuy@gmail.com>
2021-09-05 13:02:57 +0100

    the users page completed

--- dashboard-frontend/src/App.js
+++ dashboard-frontend/src/App.js
@@ -3,13 +3,9 @@ import Sidbar from './components/sidebar/Sidebar'
 import './app.css'
 import Home from "./pages/home/Home"
 import UserList from "./pages/userList/UserList"
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-}from "react-router-dom"
-
-
+import { BrowserRouter as Router, Switch, Route}from "react-router-dom"
+import User from "./pages/user/User"
+import NewUser from "./pages/newUser/NewUser"
 
 
 function App() {
@@ -24,6 +20,12 @@ function App() {
             </Route>
             <Route exact path="/users">
                 <UserList />
+            </Route>
+            <Route exact path="/user/:userId">
+                <User />
+            </Route>
+            <Route exact path="/newUser">
+                <NewUser />
             </Route>
 
         </Switch>
--- dashboard-frontend/src/components/sidebar/Sidebar.jsx
+++ dashboard-frontend/src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './sidebar.css'
+import {Link} from "react-router-dom"
 import {
   LineStyle,
   Timeline,
@@ -23,10 +24,12 @@ export default function Sidebar(){
           <div className="sidebarMenu">
               <h3 className="sidebarTitle">Dashboard</h3>
               <ul className="sidebarList">
+                  <Link to="/" className="link">
                   <li className="sidebarListItem active">
                     <LineStyle className="sidebareIcon"/>
                     Home
                   </li>
+                  </Link>
                   <li className="sidebarListItem">
                     <Timeline className="sidebareIcon"/>
                     Analytics
@@ -43,14 +46,18 @@ export default function Sidebar(){
           <div className="sidebarMenu">
               <h3 className="sidebarTitle">Quick Menu</h3>
               <ul className="sidebarList">
+                <Link to='/users' className="link" >
                   <li className="sidebarListItem ">
                     <PermIdentity className="sidebareIcon"/>
                     Users
                   </li>
+                  </Link>
+                  <Link to="/products" className="link">
                   <li className="sidebarListItem">
                     <Storefront className="sidebareIcon"/>
                     Products
                   </li>
+                  </Link>
                   <li className="sidebarListItem">
                     <AttachMoney className="sidebareIcon" />
                     Transactions
